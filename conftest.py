ceff27df5ca0031462be819a347d69fc043daa92
Hustle <real4meal@gmail.com>
2025-04-16 00:11:33 +0500

    Some changes

--- conftest.py
+++ conftest.py
@@ -1,8 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
+
+@pytest.fixture()
+def wait(driver):
+    return WebDriverWait(driver, 10)
